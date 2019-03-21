33c536d5a7a84c70f9685abdbea27b39082fdefb
Alexander Kluth <hello@dittusch.dev>
2019-03-21 02:26:21 +0100

    Initial commit of version 0.1.4
    Signed-off-by: Alexander Kluth <hello@dittusch.dev>

---
+++ GottaGoFast/Framework/ModConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace GottaGoFast.Framework
+{
+    internal class ModConfig
+    {
+        public int RunningSpeed { get; set; } = 7;
+        public int DefaultSpeed { get; set; } = 2;
+        public bool ShowHUDMessage { get; set; } = true;
+    }
+}
---
+++ GottaGoFast/ModEntry.cs
@@ -0,0 +1,67 @@
+﻿using GottaGoFast.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace GottaGoFast
+{
+    public class ModEntry : Mod
+    {
+        private ModConfig Config;
+
+        private readonly SButton sprintKey = SButton.RightShoulder;
+        private bool isAlreadyGoingFast = false;
+        private int addedSpeed;
+
+        public override void Entry(IModHelper helper)
+        {
+            this.Config = helper.ReadConfig<ModConfig>();
+            this.addedSpeed = this.Config.DefaultSpeed;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            Helper.Input.Suppress(sprintKey);
+
+            if (!Context.IsPlayerFree)
+            {
+                return;
+            }
+
+            if (e.Button.Equals(sprintKey))
+            {
+                if (!isAlreadyGoingFast)
+                {
+                    isAlreadyGoingFast = true;
+                    addedSpeed = this.Config.RunningSpeed;
+
+                    if (this.Config.ShowHUDMessage)
+                    {
+                        Game1.addHUDMessage(new HUDMessage("Schnelles Laufen aktiviert."));
+                    }
+                }
+
+                else if (isAlreadyGoingFast)
+                {
+                    isAlreadyGoingFast = false;
+                    addedSpeed = this.Config.DefaultSpeed;
+
+                    if (this.Config.ShowHUDMessage)
+                    {
+                        Game1.addHUDMessage(new HUDMessage("Schnelles Laufen deaktiviert."));
+                    }
+                }
+            }
+        }
+
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (Context.IsPlayerFree)
+            {
+                Game1.player.addedSpeed = addedSpeed;
+            }
+        }
+    }
+}
